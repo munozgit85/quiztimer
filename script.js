1ef9264b0b42035fa4603c21f4d5640d317bd70d
munozgit85 <107218022+munozgit85@users.noreply.github.com>
2022-08-05 16:08:13 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// What can be placed in a javascript array? 
+//1. Boolean
+//2. Numbers and strings 
+//3. Special characters
+//4. All of the above 
+
+// What are the different ways a function can be declared in Javascript? 
+//1. Function Expression
+//2. Constructor Functions
+//3. Getter Functions
+//4. Hoisting
+
+//What is an object in javascript?
+//1.Collection of related data and/or functionality. These usually consist of several variables and functions
+//2.Contains information about the browser.
+//3. Marks a block of statements to be executed while a condition is true
+//4. Declares a variable
+
+//What are the different types of string methods?
+//1. charAt()
+//2. constructor
+//3. repeat()
+//4. All of the above
