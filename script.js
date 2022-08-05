6f029f68500facfc04079284c0106a4d89f276f5
munozgit85 <107218022+munozgit85@users.noreply.github.com>
2022-08-05 16:34:24 -0500

    add

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+var timerEl = document.getElementById('time_in_sec');
+
+
+function time_in_sec() {
+    var timeLeft = 120;
+  
+   
+    var timeInterval = setInterval(function() {
+     
+      if (timeLeft > 1) {
+        timerEl.textContent = timeLeft + ' seconds remaining';
+        timerEl.textContent = timeLeft + ' seconds remaining';
+        timeLeft--;
+      else if (timeLeft === 1) {
+        timerEl.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+
+      else {
+        timerEl.textContent = '';
+        clearInterval(timeInterval);
+        displayMessage();
+      }
+    }, 1000);
+
+  }
+
+time_in_sec();
+
 // What can be placed in a javascript array? 
 //1. Boolean
 //2. Numbers and strings 
